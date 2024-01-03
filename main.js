3de7fa2ca0e05de5eaba4c142c51988ececf83b3
LEANDRO BUENO DA SILVA <148722703+Le-Bueno@users.noreply.github.com>
2024-01-03 11:03:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ function atualizaMediaFInal() {
 
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
 
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 
